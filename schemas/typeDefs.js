a3184e288aab3604754ff3f768249e26e2f098ba
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-11-13 16:52:29 -0600

    connected graphql

---
+++ schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+  }
+
+  type Mutation {
+    addUser(name: String!): User
+    removeUser(userId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
