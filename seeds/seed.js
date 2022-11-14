111f150dc895b146d59e430e84177b7948693ed1
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-11-13 19:35:58 -0600

    fixed database seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,11 +2,12 @@ const db = require('../config/connection');
 const { User } = require('../models');
 const userSeed = require('./userSeed.json');
 
+console.log(userSeed)
+
 db.once('open', async () => {
   try {
     await User.deleteMany({});
-    await User.create(userSeed);
-
+    await User.insertMany(userSeed.users)
     console.log('all done!');
     process.exit(0);
   } catch (err) {
