161743327346c7725dec62032a34c9dcdd1d2050
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-18 21:28:39 -0600

    logout working and redirecting properly!

--- utils/auth.js
+++ utils/auth.js
@@ -32,5 +32,7 @@ module.exports = {
   signToken: function ({  username, _id }) {
     const payload = { username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
+
+
 };
