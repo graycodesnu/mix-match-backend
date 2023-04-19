6da332a168b7b4137e886a43ddf7789a7683a057
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-18 19:09:30 -0500

    linked db

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/finalProject',
+  process.env.MONGODB_URI || 'mongodb+srv://graycodesnu:XLtpPHx3qvjrSXOZ@cluster0.a4fhmmz.mongodb.net/test',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
